f3e397987af900a34dbc58127df0ea09acc62dbb
Wilbertbh-Tan <wilbebrtbh.tan@gmail.com>
2025-06-06 15:34:06 -0400

    updated lint

--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,8 @@
+"""Unit tests for hello module."""
 from hello import add
 
-
 def test_add():
+    """Test something in hello."""
     assert add(2, 3) == 5
     assert add(5, 5) == 10
     print("All test cases passed")
